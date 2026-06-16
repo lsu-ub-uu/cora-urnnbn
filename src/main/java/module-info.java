da0c62498508bbfb87ff2fe97b775396bdd7903e
olovm <olov.mckie@ub.uu.se>
2026-06-16 12:38:12 +0000

    initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+
+module se.uu.ub.cora.urnnbn {
+	requires se.uu.ub.cora.data;
+	requires java.net.http;
+	requires jakarta.servlet;
+	requires java.base;
+	requires se.uu.ub.cora.logger;
+	requires jakarta.ws.rs;
+	requires se.uu.ub.cora.initialize;
+
+}
---
+++ src/main/java/se/uu/ub/cora/urnnbn/UrnNbnEndpoint.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2015, 2016, 2018, 2021, 2024, 2025 Uppsala University Library
+ *
+ * This file is part of Cora.
+ *
+ *     Cora is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     Cora is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with Cora.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package se.uu.ub.cora.urnnbn;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.HttpHeaders;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.UriInfo;
+import se.uu.ub.cora.logger.Logger;
+import se.uu.ub.cora.logger.LoggerProvider;
+
+@Path("")
+public class UrnNbnEndpoint {
+	private static final String DSADS = "dsads";
+	private static final String APPLICATION_XML = "application/xml";
+	HttpServletRequest request;
+	private Logger log = LoggerProvider.getLoggerForClass(UrnNbnEndpoint.class);
+
+	public UrnNbnEndpoint(@Context HttpServletRequest req) {
+		request = req;
+	}
+
+	@GET
+	@Path("")
+	@Produces({ APPLICATION_XML })
+	public Response readRecordJson(@Context UriInfo uriInfo) {
+		// String serviceName = SettingsProvider.getSetting("serviceName");
+		return Response.status(Response.Status.OK).header(HttpHeaders.CONTENT_TYPE, APPLICATION_XML)
+				.entity("<trams>" + uriInfo.getPath() + "</trams>").build();
+	}
+
+}
